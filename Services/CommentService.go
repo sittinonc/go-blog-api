020469f6b7db37ba9a53cc57162f8c304eef0f24
Grapeka <chotwilaiwan@gmail.com>
2024-03-02 15:53:59 +0700

    feat: comment service implementation

---
+++ Services/CommentService.go
@@ -0,0 +1,37 @@
+package services
+
+import (
+	"web-api/Models/DatabaseModels"
+	"web-api/Repositories/GormPsqlRepo"
+)
+
+type CommentService struct {
+	commentRepo GormPsqlRepo.CommentRepository
+}
+
+type ICommentService interface {
+	CreateComment(comment DatabaseModels.Comment) error
+	GetCommentByID(id uint) (DatabaseModels.Comment, error)
+	UpdateComment(comment DatabaseModels.Comment) error
+	DeleteComment(id uint) error
+}
+
+func NewCommentService(commentRepo GormPsqlRepo.CommentRepository) ICommentService {
+	return &CommentService{commentRepo}
+}
+
+func (cs *CommentService) CreateComment(comment DatabaseModels.Comment) error {
+	return cs.commentRepo.CreateComment(comment)
+}
+
+func (cs *CommentService) GetCommentByID(id uint) (DatabaseModels.Comment, error) {
+	return cs.commentRepo.GetCommentByID(id)
+}
+
+func (cs *CommentService) UpdateComment(comment DatabaseModels.Comment) error {
+	return cs.commentRepo.UpdateComment(comment)
+}
+
+func (cs *CommentService) DeleteComment(id uint) error {
+	return cs.commentRepo.DeleteComment(id)
+}
